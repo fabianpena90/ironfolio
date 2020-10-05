7667ba57e4828468d3a46cb4372b87a91d1f2439
Fabian Pena <camilo.fabis@gmail.com>
2020-10-05 17:05:19 -0400

    boiler plate added

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <h1>Ironfolio</h1>
+  );
 }
 
 export default App;
