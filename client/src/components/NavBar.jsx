b65cbe0a7f430c22a12b24736b845e4a4296e7c8
Rabiul Alam <rabiul.alam@outlook.com>
2020-10-05 21:54:11 -0400

    Added side nav bar, need some work

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 import "./NavBar.css";
 import Avatar from "@material-ui/core/Avatar";
+import Paper from "@material-ui/core/Paper";
+import MenuItem from "@material-ui/core/MenuItem";
+import MenuList from "@material-ui/core/MenuList";
+import { makeStyles } from "@material-ui/core/styles";
+import Divider from "@material-ui/core/Divider";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    height: "80vh",
+  },
+  paper: {
+    marginRight: theme.spacing(2),
+  },
+}));
 
 const NavBar = () => {
+  const classes = useStyles();
+
   return (
     <div>
       <div className="navTopBarContainer">
@@ -12,6 +30,22 @@ const NavBar = () => {
         <div className="profilePic">
           <Avatar src="/broken-image.jpg" />
         </div>
+      </div>
+
+      <div className={classes.root}>
+        <Paper className={classes.paper}>
+          <MenuList>
+            <MenuItem>Profile</MenuItem>
+            <Divider />
+            <MenuItem>Archive</MenuItem>
+            <Divider />
+            <MenuItem>Favorites</MenuItem>
+            <Divider />
+            <MenuItem>IronQueue</MenuItem>
+            <Divider />
+            <MenuItem>Logout</MenuItem>
+          </MenuList>
+        </Paper>
       </div>
     </div>
   );
