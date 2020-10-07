1240c728aac52cfe1b3f4d3d3b52b861c60c7d9b
Fabian Pena <camilo.fabis@gmail.com>
2020-10-07 17:50:50 -0400

    last changes before 6

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component, Fragment, useState, useEffect } from "react";
 import { Switch, Route, NavLink, useHistory } from "react-router-dom";
 import "./App.css";
 //Importing Components
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import Archive from "./components/Archive";
 import ArchiveDetail from "./components/ArchiveDetail";
@@ -61,7 +61,7 @@ function App() {
               render={(props) => <ArchiveDetail {...props} />}
             />
             <Route exact path="/favorites" render={() => <Favorites />} />
-            <Route component={NotFound} />
+            {/* <Route component={NotFound} /> */}
           </Switch>
         </div>
       </div>
@@ -69,7 +69,7 @@ function App() {
       {!user && <GoogleAuthLogin setUser={setUser} />}
       <NotificationContainer />
       </TheContext.Provider>
-      {/* <Footer /> */}
+      <Footer />
     </div>
   );
 }
