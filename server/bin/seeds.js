ce71222051855d6f0cacc2f63f38c778d98e5c53
Rabiul Alam <rabiul.alam@outlook.com>
2020-10-08 13:00:26 -0400

    Option menu show

--- server/bin/seeds.js
+++ server/bin/seeds.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Classes = require("../models/Class");
 const Teacher = require("../models/Teacher");
 const MONGODB_URI =
@@ -9,43 +9,43 @@ const classes = [
   {
     location: "MIA",
     month: "AUG",
-    year: "2020"
+    year: "2020",
   },
   {
     location: "MIA",
     month: "OCT",
-    year: "2020"
+    year: "2020",
   },
   {
     location: "MIA",
     month: "JAN",
-    year: "2021"
+    year: "2021",
   },
   {
     location: "MIA",
     month: "MAR",
-    year: "2021"
+    year: "2021",
   },
   {
     location: "MIA",
     month: "MAY",
-    year: "2021"
+    year: "2021",
   },
   {
     location: "MIA",
     month: "AUG",
-    year: "2021"
-  }
+    year: "2021",
+  },
 ];
 
-const teacher = [
-  {
-    name: "Fabian"
-  },
-  {
-    name: "Rabiul"
-  }
-];
+// const teacher = [
+//   {
+//     name: "Fabian"
+//   },
+//   {
+//     name: "Rabiul"
+//   }
+// ];
 
 mongoose
   .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -54,6 +54,6 @@ mongoose
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
     );
     Classes.insertMany(classes);
-    Teacher.insertMany(teacher);
+    // Teacher.insertMany(teacher);
   })
   .catch((err) => console.error("Error connecting to mongo", err));
