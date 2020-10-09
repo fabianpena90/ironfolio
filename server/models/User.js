c018755b02a786173c8f9ebde4aa9f1354b7af96
Fabian Pena <camilo.fabis@gmail.com>
2020-10-09 13:51:17 -0400

    solved new projects issue

--- server/models/User.js
+++ server/models/User.js
@@ -1,52 +1,94 @@
-const { Schema, model } = require("mongoose");
+const {
+  Schema,
+  model
+} = require("mongoose");
 const PLM = require("passport-local-mongoose");
 
-const userSchema = new Schema(
-  {
-    email: String,
-    name: String,
-    googleId: String,
-    imageUrl: String,
-    class: { type: String, default: "Test" },
-    userType: {
+const userSchema = new Schema({
+  email: String,
+  name: String,
+  googleId: String,
+  imageUrl: String,
+  class: {
+    type: String,
+      default: "Test"
+  },
+  userType: {
+    type: String,
+    enum: ["student", "admin"],
+    default: "student",
+  },
+  project1: {
+    name: {
       type: String,
-      enum: ["student", "admin"],
-      default: "student",
+      default: ""
     },
-    projects: {
-      project1: {
-        name: { type: String, default: "" },
-        url: { type: String, default: "" },
-        description: { type: String, default: "" },
-      },
-      project2: {
-        name: { type: String, default: "" },
-        url: { type: String, default: "" },
-        description: { type: String, default: "" },
-      },
-      project3: {
-        name: { type: String, default: "" },
-        url: { type: String, default: "" },
-        description: { type: String, default: "" },
-      },
-      project4: {
-        name: { type: String, default: "" },
-        url: { type: String, default: "" },
-        description: { type: String, default: "" },
-      },
-      project5: {
-        name: { type: String, default: "" },
-        url: { type: String, default: "" },
-        description: { type: String, default: "" },
-      },
+    url: {
+      type: String,
+      default: ""
+    },
+    description: {
+      type: String,
+      default: ""
     },
   },
-  {
-    timestamps: true,
-    versionKey: false,
-  }
-);
+  project2: {
+    name: {type: String,default: ""},
+    url: {type: String,default: ""},
+    description: {
+      type: String,
+      default: ""
+    },
+  },
+  project3: {
+    name: {
+      type: String,
+      default: ""
+    },
+    url: {
+      type: String,
+      default: ""
+    },
+    description: {
+      type: String,
+      default: ""
+    },
+  },
+  project4: {
+    name: {
+      type: String,
+      default: ""
+    },
+    url: {
+      type: String,
+      default: ""
+    },
+    description: {
+      type: String,
+      default: ""
+    },
+  },
+  project5: {
+    name: {
+      type: String,
+      default: ""
+    },
+    url: {
+      type: String,
+      default: ""
+    },
+    description: {
+      type: String,
+      default: ""
+    },
+  },
+}, {
+  timestamps: true,
+  versionKey: false,
+});
 
-userSchema.plugin(PLM, { usernameField: "email" });
+userSchema.plugin(PLM, {
+  usernameField: "email"
+});
 
 module.exports = model("User", userSchema);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -112,24 +112,24 @@ router.post("/addClass", verifyToken, (req, res) => {
 router.post("/newProject", verifyToken, (req, res) => {
   console.log("From Line 118: ", req.body);
   jwt.verify(req.token, "secretkey", (err, authData) => {
-    console.log("From Line 120: ", authData);
+    console.log("From Line 120: ", authData.user);
     if (err) {
       res.status(403).json(err);
     } else {
+      let project = req.body.project;
       User.findByIdAndUpdate(
         authData.user._id,
-        {
-          "projects.$a": {
+        { 
+          [project]: {
             name: req.body.projectName,
             url: req.body.website,
             description: req.body.description,
-          },
+          }
         },
-
         { new: true }
       ).then((project) => {
-        condolr.log("Fabian & Rabiul are the shit");
-        res.json({ project });
+        console.log("Fabian & Rabiul are the shit");
+        res.json( project );
       });
     }
   });
