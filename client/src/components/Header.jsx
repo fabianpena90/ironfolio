ede5df85cda87e52dc3813a105d950c68bb9a71a
Rabiul Alam <rabiul.alam@outlook.com>
2020-10-09 08:44:39 -0400

    Profile pic adjusted

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
 import Avatar from "@material-ui/core/Avatar";
+import { makeStyles } from '@material-ui/core/styles';
+import TheContext from "../TheContext";
+import './Header.css'
 
 
 function Header(props) {
+  const { user} = React.useContext(TheContext);
+  console.log(user)
+
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      display: 'flex',
+      '& > *': {
+        margin: theme.spacing(1),
+      },
+    },
+  }));
+
+  const classes = useStyles();
   return (
       <div className="navTopBarContainer">
         <div className="logo">
@@ -11,8 +27,9 @@ function Header(props) {
             <h1>Ironfolio</h1>
           </a>
         </div>
-        <div className="profilePic">
-          <Avatar src="/broken-image.jpg" />
+        <div className={classes.root}>
+          <h3>{user?.name}</h3>
+          <Avatar src={user?.imageUrl} />
         </div>
       </div>
   );
