ebec22a6e36a8ba0f8fcb0dae91f1edd32372fa4
Fabian Pena <camilo.fabis@gmail.com>
2020-10-14 11:15:08 -0400

    deploying

--- server/app.js
+++ server/app.js
@@ -28,8 +28,8 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:3000"],
-    // "https://clientnetlify.netlify.app"
+    origin: ["http://localhost:3000", "https://iron-folio.netlify.app"],
+    
     //Swap this with the client url
   })
 );
