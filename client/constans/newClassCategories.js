341a97585bac34e79aeae58cfd5b631afafd271d
Rabiul Alam <rabiul.alam@outlook.com>
2021-01-10 17:47:42 -0500

    Added constants

---
+++ client/constans/newClassCategories.js
@@ -0,0 +1,31 @@
+export const campuses = [
+    { campus: 'Miami', code: 'MIA' },
+];
+
+export const months = [
+    { month: 'January', code: 'JAN' },
+    { month: 'February', code: 'FEB' },
+    { month: 'March', code: 'MAR' },
+    { month: 'April', code: 'APR' },
+    { month: 'May', code: 'MAY' },
+    { month: 'June', code: 'JUN' },
+    { month: 'July', code: 'Jul' },
+    { month: 'August', code: 'AUG' },
+    { month: 'September', code: 'SEP' },
+    { month: 'October', code: 'OCT' },
+    { month: 'November', code: 'NOV' },
+    { month: 'December', code: 'DEC' },
+];
+
+let year = new Date().getFullYear();
+
+
+export const years = [
+    { year: year},
+    { year: year+1 },
+];
+
+export const classType = [
+    { type: "Full Time", code: "FT" },
+    {type: "Part Time", code: "PT"},
+]
