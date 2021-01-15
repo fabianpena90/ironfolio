8d1f94243b1165cc7f356a6ad6ffa574209342f3
Rabiul Alam <rabiul.alam@outlook.com>
2021-01-14 23:03:52 -0500

    Add yup for frontend form validation,Page-Addnew

--- client/src/components/AddNew.jsx
+++ client/src/components/AddNew.jsx
@@ -1,24 +1,26 @@
-import React, { useState, useEffect } from "react";
-import "./AddNew.css";
-import actions from "../api/index";
-import TheContext from "../TheContext";
+import React, { useState, useContext } from 'react';
+import './AddNew.css';
+import actions from '../api/index';
+import TheContext from '../TheContext';
+import * as yup from 'yup';
+import {
+  NotificationManager,
+} from 'react-notifications';
 
 // Material UI
-import { makeStyles } from "@material-ui/core/styles";
-import TextField from "@material-ui/core/TextField";
-import Button from "@material-ui/core/Button";
-import InputLabel from "@material-ui/core/InputLabel";
-import Select from "@material-ui/core/Select";
-import FormControl from "@material-ui/core/FormControl";
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import FormControl from '@material-ui/core/FormControl';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(5),
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "spaceBetween",
-    alignItems: "center",
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'spaceBetween',
+    alignItems: 'center',
   },
   selectEmpty: {
     marginTop: theme.spacing(5),
@@ -27,33 +29,48 @@ const useStyles = makeStyles((theme) => ({
 
 const AddNew = (props) => {
   const classes = useStyles();
-  const { user, history } = React.useContext(TheContext);
-  const [project, setProject] = useState();
+  const { user, history } = useContext(TheContext);
   const [projectName, setProjectName] = useState();
   const [description, setDescription] = useState();
   const [website, setWebsite] = useState();
+  console.log(website);
 
-  async function addProjects() {
-    let res = await actions.addProject({
-      class: user.class,
-      project,
-      projectName,
-      description,
-      website,
-    });
-    //console.log(res, "Fabian & Rabiul are the shit!");
+  const projectSchema = yup.object().shape({
+    projectName: yup.string().max(24),
+    description: yup.string().max(255),
+    website: yup.string().url(),
+  });
+  
+  String.prototype.capitalize = function() {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+}
+
+  function addProjects() {
+    projectSchema
+      .validate({
+        projectName,
+        description,
+        website,
+      })
+      .then((data) => {
+        actions.addProject(data);
+        history.push('/');
+      })
+      .catch((err) => {
+        NotificationManager.error(err.errors[0].capitalize(), 'Error', 4000, true);
+        console.log(err.errors[0]);
+      });
   }
 
-  if (props.user.class === "Test") {
-    history.push("/");
+  if (props.user.class === 'Test') {
+    history.push('/');
   }
   if (!user.email) {
-    history.push("/login");
+    history.push('/login');
   }
   const handleSubmit = (e) => {
+    e.preventDefault();
     addProjects();
-    history.push("/");
-    //e.preventDefault();
   };
 
   return (
@@ -93,6 +110,7 @@ const AddNew = (props) => {
               name="website"
               label="Website"
               variant="outlined"
+              placeholder="https://www.google.com"
             />
           </FormControl>
           <TextField
@@ -106,21 +124,21 @@ const AddNew = (props) => {
             }}
             variant="outlined"
             required="true"
+            placeholder="max 255 characters..."
             fullWidth
             rows={8}
           />
           <Grid container justify="center">
-          <Button
-            color="secondary"
-            className="btnAdd"
-            size="large"
-            variant="contained"
-            type="submit"
-          >
-            Submit
-          </Button>
-</Grid>
-          
+            <Button
+              color="secondary"
+              className="btnAdd"
+              size="large"
+              variant="contained"
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Grid>
         </form>
       </div>
     </div>
