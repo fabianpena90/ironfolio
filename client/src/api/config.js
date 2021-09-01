2d37a1b93500c23b5ab4500daad4a2a1cdc2c1a6
Rabiul Alam <rabiul.alam@outlook.com>
2021-09-01 08:21:13 -0400

    Fixed Backend URI

--- client/src/api/config.js
+++ client/src/api/config.js
@@ -1,12 +1,12 @@
 let baseURL;
 if (process.env.NODE_ENV === 'production') {
   if (process.env.REACT_APP_API) {
-    baseURL = `http://44.192.254.67:5000/api`; //Netlify
+    baseURL = `${process.env.REACT_APP_API}/api`; //Netlify
   } else {
     baseURL = `/api`; //Heroku or 5000
   }
 } else {
-  baseURL = `http://44.192.254.67:5000/api`; //http://localhost:5000/apihttps://ironfolio.herokuapp.com/api
+  baseURL = `http://localhost:5000/api`; //http://localhost:5000/apihttps://ironfolio.herokuapp.com/api
 }
 // baseURL = `https://pacific-river-72403.herokuapp.com/api`;
 
